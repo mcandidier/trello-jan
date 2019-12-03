a8e7ac4ed77f6b36d173a107ccda6a0415378525
jvallno <jvallno23@gmail.com>
2019-12-03 23:16:20 +0800

    models updates

--- app/models.py
+++ app/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from django.conf import settings
+from django.contrib.auth.models import User
 from django.utils import timezone
 
 class Board(models.Model):
     title = models.CharField(max_length=30)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     date_created = models.DateField(default=timezone.now)
 
 class BoardList(models.Model):
