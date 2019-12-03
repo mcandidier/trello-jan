6e7f1e792b5526ddc56200ce33cbd6e7abb25d9a
jvallno <jvallno23@gmail.com>
2019-12-03 15:20:11 +0800

    update model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
-# Create your models here.
+class TrelloBoard(models.Model):
+    title = models.CharField(max_length=30)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date_created = models.DateField(default=timezone.now)
+
+class TrelloList(models.Model):
+    title = models.CharField(max_length=30)
+    date_created = models.DateField(default=timezone.now)
+    trello_board = models.ForeignKey(TrelloBoard, on_delete=models.CASCADE)
+
+class TrelloCard(models.Model):
+    title = models.CharField(max_length=30)
+    date_created = models.DateField(default=timezone.now)
+    trello_list = models.ForeignKey(TrelloList, on_delete=models.CASCADE)
