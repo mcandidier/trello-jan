43541c6fdafb8929fa4b732e298a7a41ef6047e6
jvallno <jvallno23@gmail.com>
2019-12-03 23:15:48 +0800

    forms updates

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,13 @@ from django import forms
 from .models import Board, Card, BoardList
 
 class BoardForm(forms.ModelForm):
-    
+
     class Meta:
         model = Board
+        fields = ('title',)
+
+class CardForm(forms.ModelForm):
+
+    class Meta:
+        model = BoardList
         fields = ('title',)
