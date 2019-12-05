8ec87a5d18fc66eee0544c00b2cc20e8bd201e98
jvallno <jvallno23@gmail.com>
2019-12-05 23:53:20 +0800

    url updates

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from app.views import Boards, AddBoard, BoardView, AddList
+from app.views import Boards, AddBoard, BoardView, AddList, AddCard
 from . import views
 
 urlpatterns = [
     path('', Boards.as_view()),
     path('board/new/', AddBoard.as_view(), name='board_new'),
     path('board/<int:id>/', BoardView.as_view(), name='board_view'),
-    path('list/new/<int:id>', AddList.as_view(), name='list_new'),
+    path('board/<int:id>/list/new', AddList.as_view(), name='list_new'),
+    path('board/list/card/new/<int:id>', AddCard.as_view(), name='card_new'),
 ]
